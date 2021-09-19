a686c323be977cfb4d20dac22ea74895f27d550d
Ivan Volkov <volkivanv@mail.ru>
2021-09-19 23:43:44 +0300

    Second Commit

--- main.cpp
+++ main.cpp
@@ -7,16 +7,24 @@ int main() {
     std::string filepath;
     std::cout<<"Please, input filepath!" << std::endl;
     getline(std::cin, filepath);
-    //, std::ios::binary
-    notebook.open(filepath);
+
+    notebook.open(filepath, std::ios::binary);
     if(notebook) {
         while (!notebook.eof()) {
-            std::string bufStr;
-            getline(notebook,bufStr);
-            std::cout << bufStr;
-          //  char buffer[30];
-          //  notebook.read(buffer, sizeof(buffer));
-          //  std::cout << buffer.;
+          //  std::string bufStr;
+          //  getline(notebook,bufStr);
+          //  std::cout << bufStr;
+            char buffer[30];
+            notebook.read(buffer, sizeof(buffer));
+            if(notebook.gcount() == sizeof(buffer)){
+                std::cout << buffer;
+            } else{
+                for(int i = 0; i < notebook.gcount(); i++){
+                    std::cout << buffer[i];
+                }
+
+            }
+
         }
         notebook.close();
     }else {
